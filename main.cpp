438e46a2ecf7c547ad52a1e7c6fd048a32a15d40
dcfrenci <dcfrenci@gmail.com>
2025-05-29 08:14:22 +0200

    cmake

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <sstream>
+
+auto get_compiler_info() {
+    std::stringstream str;
+
+#if defined(__clang__)
+    str << "clang " << __clang_major__ << '.' << __clang_minor__ << '.' << __clang_patchlevel__;
+#elif defined(__GNUC__) && !defined(__ICC)
+    str << "gcc " << __GNUC__ << '.' << __GNUC_MINOR__ << '.' << __GNUC_PATCHLEVEL__;
+#elif defined(_MSC_VER)
+    str << "msvc " << _MSC_VER;
+#elif defined(__ICC)
+    str << "icc " << __VERSION__;
+#endif
+
+    return str.str();
+}
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
+    const auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
     for (int i = 1; i <= 5; i++) {
@@ -15,7 +32,7 @@ int main() {
         // <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
     }
-
+    std::cout << get_compiler_info() << '\n';
     return 0;
 }
 
