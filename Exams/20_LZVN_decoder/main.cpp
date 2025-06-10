1a33a8c1310c91502158e85ea434603feb40e9e0
dcfrenci <dcfrenci@gmail.com>
2025-06-10 12:01:05 +0200

    Exams 20 LZVN

--- Exams/20_LZVN_decoder/main.cpp
+++ Exams/20_LZVN_decoder/main.cpp
@@ -1,105 +1,147 @@
+//time 04:00:00
+
 #include <cstdint>
 #include <fstream>
-#include <iosfwd>
-#include <bit>
 #include <iostream>
 
-struct bit_reader {
-    uint8_t buffer_;
-    uint8_t size_;
-    std::ifstream &input;
+uint32_t fit(const uint32_t value, const uint32_t min, const uint32_t max) {
+    return value >= min and value <= max ? value : min;
+}
 
-    explicit bit_reader(std::ifstream &input) : buffer_(0), size_(0), input(input) {
+int calculate(std::ifstream &input, const uint8_t &byte, std::string &literals, uint16_t &match_distance,
+              uint16_t &match_length) {
+    std::string string = "0000";
+    uint8_t literals_length = 0;
+    if (byte == 0b00011110 or byte == 0b00100110 or byte == 0b00101110 or byte == 0b00110110 or byte == 0b00111110) {
+        //udef
+        return EXIT_FAILURE;
+    }
+    if (byte == 0b00001110 or byte == 0b00010110) {
+        //nop
+        return EXIT_SUCCESS;
+    }
+    if (byte == 0b11100000) {
+        //lrg_l
+        input.read(reinterpret_cast<char *>(&literals_length), 1);
+        literals_length += 16;
+        string.resize(literals_length);
+        input.read(string.data(), literals_length);
+        literals += string;
+        return 2;
+    }
+    if (byte == 0b00000110) {
+        //eos
+        uint8_t bytes[7];
+        input.read(reinterpret_cast<char *>(bytes), 7);
+        return -1;
+    }
+    if (byte == 0b11110000) {
+        //lrg_m
+        uint8_t byte2;
+        input.read(reinterpret_cast<char *>(&byte2), 1);
+        match_length = byte2 + 16;
+    } else if ((byte & 0b11110000) == 0b01110000 or (byte & 0b11110000) == 0b11010000) {
+        //udef
+        return EXIT_FAILURE;
+    } else if ((byte & 0b11110000) == 0b11110000) {
+        //sml_m
+        match_length = byte & 0b00001111;
+    } else if ((byte & 0b11100000) == 0b11100000) {
+        //sml_l
+        literals_length = byte & 0b00001111;
+        string.resize(literals_length);
+        input.read(string.data(), literals_length);
+        literals += string;
+        return 2;
+    } else if ((byte & 0b11100000) == 0b10100000) {
+        //med_d
+        uint8_t byte2, byte3;
+        input.read(reinterpret_cast<char *>(&byte2), 1);
+        input.read(reinterpret_cast<char *>(&byte3), 1);
+        literals_length = byte >> 3 & 0b00000011;
+        match_length = ((byte & 0b00000111) << 2 | byte2 & 0b00000011) + 3;
+        match_distance = byte3 << 6 | byte2 >> 2;
+    } else if ((byte & 0b00000111) == 0b00000111) {
+        //lrg_d
+        uint8_t byte2, byte3;
+        input.read(reinterpret_cast<char *>(&byte2), 1);
+        input.read(reinterpret_cast<char *>(&byte3), 1);
+        literals_length = byte >> 6;
+        match_length = (byte >> 3 & 0b00000111) + 3;
+        match_distance = byte3 << 8 | byte2;
+    } else if ((byte & 0b00000111) == 0b00000110) {
+        //pre_d
+        literals_length = byte >> 6;
+        match_length = (byte >> 3 & 0b00000111) + 3;
+    } else {
+        //sml_d
+        uint8_t byte2;
+        input.read(reinterpret_cast<char *>(&byte2), 1);
+        literals_length = byte >> 6;
+        match_length = (byte >> 3 & 0b00000111) + 3;
+        match_distance = (byte & 0b00000111) << 8 | byte2;
     }
 
-    uint8_t read_bit() {
-        if (size_ == 0) {
-            input.read(reinterpret_cast<char *>(&buffer_), 1);
-            size_ = 8;
-        }
-        size_--;
-        return buffer_ >> size_ & 1;
+    if (literals_length > 0) {
+        string.resize(literals_length);
+        input.read(string.data(), literals_length);
+        literals += string;
     }
+    return EXIT_SUCCESS;
+}
 
-    uint32_t operator ()(uint32_t n_bits) {
-        uint32_t result = 0;
-        while (n_bits-- > 0) {
-            result = result << 1 | read_bit();
-        }
-        return result;
-    }
-};
-
-int decoder(std::ifstream &input, std::ofstream &output) {
-    bit_reader reader(input);
-    bool eof = false;
-    uint32_t l = 0, m = 0, d = 0;
-    while (!eof) {
-        const uint8_t f1_byte = reader(8);
-        if (f1_byte >> 5 & 0xFF == 0x05) {
-            // med_d
-            std::cout << "med_d\n";
-            const uint8_t f2_byte = reader(8);
-            const uint8_t f3_byte = reader(8);
-            l = f1_byte << 3 >> 3 & 0xFF;
-            const uint16_t m1 = (f1_byte << 5 & 0xFF) >> 3;
-            const uint16_t m2 = (f2_byte << 6 & 0xFF) >> 6;
-            const uint16_t d1 = (f2_byte >> 2 & 0xFF) << 8;
-            m = m1 | m2;
-            d = d1 | f3_byte;
-
-        } else if (f1_byte << 5 & 0xFF == 0xE0) {
-            // lrg_d
-            std::cout << "lrg_d\n";
-        } else if (f1_byte << 5 & 0xFF == 0xC0) {
-            // pre_d
-            std::cout << "pre_d\n";
-        } else if (f1_byte >> 4 & 0xFF == 0x0F) {
-            // sml_m
-            std::cout << "sml_m\n";
-        } else if (f1_byte == 0xF0) {
-            // lrg_m
-            std::cout << "lrg_m\n";
-        } else if (f1_byte >> 4 & 0xFF == 0x0E) {
-            // sml_l
-            std::cout << "sml_l\n";
-        } else if (f1_byte == 0xE0) {
-            // lrg_l
-            std::cout << "lrg_l\n";
-        } else if (f1_byte == 0x0E or f1_byte == 0x16) {
-            // nop
-            std::cout << "nop\n";
-        } else if (f1_byte == 0x06) {
-            // eof
-            std::cout << "eof\n";
-            eof = true;
-        } else if (f1_byte >> 4 & 0xFF == 0x07 or f1_byte >> 4 & 0xFF == 0x0D or f1_byte == 0x1E or f1_byte == 0x26 or
-                   f1_byte == 0x2E or f1_byte == 0x36 or f1_byte == 0x3E) {
-            // udef
-            std::cout << "udef\n";
-            return EXIT_FAILURE;
-        } else {
-            // sml_d
-            std::cout << "sml_d\n";
-            const uint8_t f2_byte = reader(8);
-            l = f1_byte >> 6 & 0xFF;
-            m = (f1_byte << 2 & 0xFF) >> 5 & 0xFF;
-            const uint16_t d1 = (f1_byte << 5 & 0xFF) << 3;
-            d = d1 | f2_byte;
-            auto temp = 0;
+int decode(std::ifstream &input, std::ofstream &output) {
+    while (input.good()) {
+        std::string magic_number = "code";
+        input.read(magic_number.data(), 4);
+        if (magic_number == "bvxn") {
+            uint32_t byte_number, block_size;
+            input.read(reinterpret_cast<char *>(&byte_number), 4);
+            input.read(reinterpret_cast<char *>(&block_size), 4);
+            const uint32_t block_start = input.tellg();
+            std::string literals;
+            uint16_t match_distance = 0, match_length = 0;
+            while (static_cast<uint32_t>(input.tellg()) - block_start < block_size) {
+                uint8_t byte;
+                input.read(reinterpret_cast<char *>(&byte), 1);
+                const int ret = calculate(input, byte, literals, match_distance, match_length);
+                if (ret == EXIT_FAILURE) {
+                    return EXIT_FAILURE;
+                }
+                if (ret == -1) {
+                    break;
+                }
+                if (ret != 2) {
+                    const uint32_t start = fit(literals.size() - match_distance, 0, literals.size() - 1);
+                    std::string string = literals.substr(start, literals.size());
+                    for (uint32_t i = 0; i < match_length;) {
+                        for (const auto &c: string) {
+                            if (i == match_length) {
+                                break;
+                            }
+                            literals.push_back(c);
+                            i++;
+                        }
+                    }
+                }
+            }
+            output << literals;
+        } else if (magic_number == "bvx$") {
+            break;
         }
     }
     return EXIT_SUCCESS;
 }
 
-int main(const int argc, const char *argv[]) {
+int main(const int argc, const char **argv) {
     if (argc != 3) {
         return EXIT_FAILURE;
     }
     std::ifstream input(argv[1], std::ios::binary);
     std::ofstream output(argv[2], std::ios::binary);
+    // temp(input, output);
     if (!input or !output) {
         return EXIT_FAILURE;
     }
-    return decoder(input, output);
+    return decode(input, output);
 }
